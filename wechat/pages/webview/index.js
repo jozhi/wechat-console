4fdda7504355ae8544fc69636b0725a8a605794d
jiaoshengzhi <11194720@qq.com>
2020-03-10 15:46:27 +0800

    添加事件钩子

--- wechat/pages/webview/index.js
+++ wechat/pages/webview/index.js
@@ -5,12 +5,16 @@ const app = getApp()
 const eplog = require('../../components/eplog.js')
 const http = require('../../utils/http.js')
 Page({
+  // 调用 console 组件
+  showPopup() {
+    eplog()
+  },
   /**
    * 页面的初始数据
    */
   data: {
+    
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
@@ -102,8 +106,51 @@ Page({
       }
     })
   },
-  // 调用 console 组件
-  showPopup() {
-    eplog()
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
   },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
 })
